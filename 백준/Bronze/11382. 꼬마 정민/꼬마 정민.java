8f3a153dffd3a90290f1fc664497fa211b10d98b
MinHyun-code <72852292+MinHyun-code@users.noreply.github.com>
2023-09-20 15:39:37 +0900

    [Bronze V] Title: 꼬마 정민, Time: 200 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		Long A = Long.parseLong(scan.next());
+		Long B = Long.parseLong(scan.next());
+		Long C = Long.parseLong(scan.next());
+		
+		System.out.println(A+B+C);
+	}
+}
