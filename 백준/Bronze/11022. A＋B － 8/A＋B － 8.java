324fafd9dd4bf762bcea153c8a5bdec6d0c96326
MinHyun-code <72852292+MinHyun-code@users.noreply.github.com>
2023-09-21 14:11:38 +0900

    [Bronze V] Title: A+B - 8, Time: 184 ms, Memory: 16652 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int A = Integer.parseInt(bf.readLine());
+		
+		for(int i=1; i<A+1; i++) {
+			String B = bf.readLine();
+			
+			String[] C = B.split(" ");
+			
+			bw.write("Case #"+i+": "+C[0]+" + "+C[1]+" = "+(Integer.parseInt(C[0]) + Integer.parseInt(C[1]))+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
