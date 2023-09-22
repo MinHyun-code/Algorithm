685e655b599042ca3a7d6ee279b7834111068718
MinHyun-An <72852292+MinHyun-code@users.noreply.github.com>
2023-09-22 10:00:04 +0900

    [Bronze V] Title: 오늘 날짜, Time: 204 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+	public static void main(String[] args){
+	
+		LocalDate now = LocalDate.now();
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		
+		String date = now.format(formatter);
+		
+		System.out.print(date);
+	}
+}
