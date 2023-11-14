c88de806e76af37c9ce9ee0eb70ececf27a4b859
MinHyun-An <72852292+MinHyun-code@users.noreply.github.com>
2023-11-14 17:20:12 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 14184 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		/*
+		 * 모듈러 연산의 성질
+		 * 1) (A+B) mod M = ((A mod M) + (B mod M)) mod M
+		 * 2) (A*B) mod M = ((A mod M) * (B mod M)) mod M
+		 * 
+		 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int num = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+
+		int[] arr = new int[num];
+		int sum = 0;
+		int r = 1;
+		int prime = 1234567891;
+		
+		for(int i=0; i<num; i++) {
+			
+			if(i>0) {
+				r = r*31%prime;
+			}
+			arr[i] = (str.charAt(i)-96)*r%prime;
+		}
+		
+		for(int i=0; i<num; i++) {
+			sum += arr[i];
+		}
+
+		System.out.println(sum%prime);
+	} 
+}
