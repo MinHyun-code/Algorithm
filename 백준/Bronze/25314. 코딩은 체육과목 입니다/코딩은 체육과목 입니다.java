02892470548aa5c71c4ba56258bc881a3dc5ddb5
MinHyun-An <72852292+MinHyun-code@users.noreply.github.com>
2023-11-17 14:36:01 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException{
 		
-		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
-		int A = scan.nextInt();
-
-		String result = "";
+		int num = Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<(A/4); i++) {
-			result += "long ";
+		for(int i=0; i<num/4; i++) {
+			sb.append("long").append(" ");
 		}
+		sb.append("int");
 		
-		System.out.print(result + "int");
+		System.out.println(sb);
 	}
 }
