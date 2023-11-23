4d51cb0177580586166c921e70f5508b7283ae7c
MinHyun-An <72852292+MinHyun-code@users.noreply.github.com>
2023-11-23 17:33:48 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 132 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		long n = Long.parseLong(br.readLine());
+		  
+		System.out.println(n*n*n);
+		System.out.println(3);
+	}
+}
