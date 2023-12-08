7d01ef06b2cd66d503d75661ba4bc81c5c9dc513
MinHyun-An <72852292+MinHyun-code@users.noreply.github.com>
2023-12-08 17:34:24 +0900

    [Silver II] Title: 최소 힙, Time: 328 ms, Memory: 26416 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static long result;
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+	
+		N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+		
+		while(N-->0) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num == 0) {
+				int exitNum;
+				if(queue.size() > 0) {
+					exitNum = queue.poll();
+				} else {
+					exitNum = 0;
+				}
+				sb.append(exitNum).append("\n");
+			} 
+			else {
+				queue.add(num);
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
