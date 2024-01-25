59e65840ce3692b428b0e2079c5c3e1cf4b4558c
MinHyun-An <72852292+MinHyun-code@users.noreply.github.com>
2024-01-25 15:10:09 +0900

    [Silver III] Title: 주유소, Time: 340 ms, Memory: 30180 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -12,9 +12,6 @@ public class Main {
 	static int M;
 	static int[] arrN;
 	static int[] arrM;
-	static int minValue = 1000000000;
-	static int minIndex = 100000;
-	static long amt = 0;
 	
 	public static void main(String[] args) throws IOException{
 		
@@ -36,57 +33,19 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++) {
 			arrN[i] = Integer.parseInt(st.nextToken());	
-			// 끝지점은 제외
-			if(minValue > arrN[i] && i != N) {
-				minValue = arrN[i];
-				minIndex = i;
-			}
 		}
 		
-		// 처음이 가장 작은 가격일 경우
-		if(minIndex == 1) {
-			for(int i=1; i<=M; i++) {
-				amt += arrM[i];
+		int minCost = arrN[1];
+		long amt = 0;
+		
+		for(int i=1; i<=N-1; i++) {
+			if(arrN[i] < minCost) {
+				minCost = arrN[i];
 			}
-		} 
-		else {
-			amt += (arrN[1] * arrM[1]);
-			subFunction(minIndex, N);
+			
+			amt += minCost * arrM[i];
 		}
-		// 위 조건 이전 도로는 어떻게 할까?
-		// 똑같이 반복문 돌려야할거 같은데
 		
-		
-		// 맨 끝 도시의 기름 가격은 안씀
-
 		System.out.println(amt);
-	}
-	
-	public static void subFunction(int min, int max) {
-	// 제일 작은 비용 : 뒤로 있는 도로 길이 모두 +
-		
-		long total = 0; // 도로 길이 합
-		
-		for(int i=min; i<max; i++) {
-			total += arrM[i];
-		}
-		
-		amt += (total * arrN[minIndex]);
-
-		System.out.println(amt);
-		
-		minValue = 1000000000;
-		int maxIndex = minIndex;
-		
-		for(int i=2; i<minIndex; i++) {
-			if(minValue > arrN[i]) {
-				minValue = arrN[i];
-				minIndex = i;
-			}
-		}
-		
-		if(minValue != 1000000000) {
-			subFunction(minIndex, maxIndex);
-		}
 	}
 }
